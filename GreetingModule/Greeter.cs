a0d6e47f957d0f4c7b37a81134c39252a1f2f75e
Markpepsi <aim130906@gmail.com>
2026-03-03 09:55:49 +0300

    Initial project with modules and tests

---
+++ GreetingModule/Greeter.cs
@@ -0,0 +1,8 @@
+﻿namespace GreetingModule
+{
+    public class Greeter
+    {
+        public string Greet(string name) => $"Hello, {name}!";
+    }
+}
+
---
+++ Tests/GreeterTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using GreetingModule;
+
+namespace Tests
+{
+    public class GreeterTests
+    {
+        [Fact]
+        public void Greet_ShouldReturnHello()
+        {
+            var greeter = new Greeter();
+            var result = greeter.Greet("World");
+            Assert.Equal("Hello, World!", result);
+        }
+    }
+}
+
